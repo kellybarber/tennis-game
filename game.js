1a7838b861123a15320a3c26095302100e3e19c4
kellybarber <kelly.james.barber@gmail.com>
2018-03-10 08:43:37 -0500

    created timeout and positional value variable to move and object across the screen

--- game.js
+++ game.js
@@ -1,11 +1,24 @@
 let canvas 
 let canvasContext
+let ballX = 50
 
 window.onload = () => {
   console.log('hello world')
 
   canvas = document.getElementById('gameCanvas')
   canvasContext = canvas.getContext('2d')
+
+  const framesPerSecond = 30
+  setInterval(drawEverything, 1000/framesPerSecond)
+}
+
+function drawEverything() {
+  ballX = ballX + 5
+
   canvasContext.fillStyle = 'black'
   canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+  canvasContext.fillStyle = 'orange'
+  canvasContext.fillRect(350, 250, 100, 100)
+  canvasContext.fillStyle = 'white'
+  canvasContext.fillRect(ballX, 100, 10, 10)
 }
