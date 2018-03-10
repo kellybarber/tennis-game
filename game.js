df87dfbd6fafc1a27199b767ab5515e5971e6b83
kellybarber <kelly.james.barber@gmail.com>
2018-03-10 12:34:01 -0500

    made paddle follow mouse using an event listener and helper function

--- game.js
+++ game.js
@@ -2,11 +2,24 @@ let canvas
 let canvasContext
 let ballX = 50
 let ballSpeedX = 15
+let ballY = 50
+let ballSpeedY = 15
 
+let paddleOneY = 250
+const PADDLE_HEIGHT = 100
+
+function calculateMousePosition(evt) {
+  let rect = canvas.getBoundingClientRect()
+  let root = document.documentElement
+  let mouseX = evt.clientX - rect.left - root.scrollLeft
+  let mouseY = evt.clientY - rect.top - root.scrollTop
+  return {
+    x: mouseX,
+    y: mouseY
+  }
+}
 
 window.onload = () => {
-  console.log('hello world')
-
   canvas = document.getElementById('gameCanvas')
   canvasContext = canvas.getContext('2d')
 
@@ -15,10 +28,16 @@ window.onload = () => {
     moveEverything()
     drawEverything() 
   }, 1000/framesPerSecond)
+
+  canvas.addEventListener('mousemove', (evt) => {
+    let mousePosition = calculateMousePosition(evt) 
+    paddleOneY = mousePosition.y
+  })
 }
 
 function moveEverything() {
   ballX = ballX + ballSpeedX
+  ballY = ballY + ballSpeedY
 
   if (ballX > canvas.width) {
     ballSpeedX = -ballSpeedX
@@ -26,12 +45,21 @@ function moveEverything() {
   if (ballX < 0) {
     ballSpeedX = -ballSpeedX
   }
+  if (ballY > canvas.height) {
+    ballSpeedY = -ballSpeedY
+  }
+  if (ballY < 0) {
+    ballSpeedY = -ballSpeedY
+  }
 }
 
 function drawEverything() {
+  // Game Background
   colorRect(0, 0, canvas.width, canvas.height, 'black')
-  colorRect(0, 250, 10, 100, 'white')
-  colorCircle(ballX, 150, 10, 'white')
+  // Player One Paddle
+  colorRect(0, paddleOneY, 10, 100, 'white')
+  // Game Ball
+  colorCircle(ballX, ballY, 10, 'white')
 }
 
 function colorRect(leftX, topY, width, height, drawColor) {
@@ -39,7 +67,7 @@ function colorRect(leftX, topY, width, height, drawColor) {
   canvasContext.fillRect(leftX, topY, width, height)
 }
 
-function colorCircle(centerX, centerY, radius, color) {
+function colorCircle(centerX, centerY, radius, drawColor) {
   canvasContext.fillStyle = drawColor
   canvasContext.beginPath()
   canvasContext.arc(centerX, centerY, radius, 0, Math.PI * 2, true)
